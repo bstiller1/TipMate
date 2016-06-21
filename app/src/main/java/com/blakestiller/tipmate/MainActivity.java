d5aaca65d8570420d4ea1ed9fe33c21bd24e0583
Blake Stiller <stiller.blake@gmail.com>
2016-06-21 16:16:43 -0400

    Added commenting

--- app/src/main/java/com/blakestiller/tipmate/MainActivity.java
+++ app/src/main/java/com/blakestiller/tipmate/MainActivity.java
@@ -17,24 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Locate and Identify Controls
+        // Locate and Identify Button
         Button button = (Button) findViewById(R.id.button);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View button) {
+                // Locate and Identify Controls
                 EditText editText = (EditText) findViewById(R.id.editText);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
                 EditText editText3 = (EditText) findViewById(R.id.editText3);
                 EditText editText4 = (EditText) findViewById(R.id.editText4);
+                // Convert User Input to Double
                 double amount = Double.parseDouble(editText.getText().toString());
                 double guests = Double.parseDouble(editText2.getText().toString());
                 double tip = Double.parseDouble(editText3.getText().toString());
-        Float total = (float) (Math.round((amount + tip) / guests));
+                // Do Calculation
+                Float total = (float) (Math.round((amount + tip) / guests));
+                // Format to Decimal for Money
                 DecimalFormat totalEach = new DecimalFormat("$0.00");
+                // Construct StringBuilder Object for final output
                 StringBuilder sb = new StringBuilder();
+                // Format for USD
                 Formatter formatter = new Formatter(sb, Locale.US);
-        editText4.setText(totalEach.format(total));
+                // Display final amount to user
+                editText4.setText(totalEach.format(total));
     }
     });
 
